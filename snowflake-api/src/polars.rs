5e8daff03817f83fc3af2b4ed7b4ac93bbda76d4
tobisinghania <tobisinghania@gmail.com>
2024-12-24 13:54:12 +0100

    Increase minimum polars-rs version to 0.41 (#49)
    * Increase minimum polars-rs version to 0.41
    
    * Clippy polars.rs
    
    ---------
    
    Co-authored-by: Tobias Singhania <tobias.singhania@vontobel.com>
    Co-authored-by: andrusha <korzhuev@andrusha.me>

--- snowflake-api/src/polars.rs
+++ snowflake-api/src/polars.rs
@@ -1,5 +1,5 @@
 use std::convert::TryFrom;
-use std::num::NonZero;
+use std::num::NonZeroUsize;
 
 use bytes::{Buf, Bytes};
 use polars_core::frame::DataFrame;
@@ -38,7 +38,7 @@ fn dataframe_from_json(json_result: &JsonResult) -> Result<DataFrame, PolarsCast
     let reader = std::io::Cursor::new(json_string.as_bytes());
     let df = JsonReader::new(reader)
         .with_json_format(JsonFormat::Json)
-        .infer_schema_len(Some(NonZero::new(5).unwrap()))
+        .infer_schema_len(NonZeroUsize::new(5))
         .finish()?;
     Ok(df)
 }
